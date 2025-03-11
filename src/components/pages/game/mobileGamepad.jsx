3f9f4d8aeb4e707fa3e67e319375a44a43774053
AndersonBillss <anderson@thebillsfamily.com>
2025-03-10 23:08:54 -0600

    finished styling gamepad component

--- src/components/pages/game/mobileGamepad.jsx
+++ src/components/pages/game/mobileGamepad.jsx
@@ -1,31 +1,55 @@
 import React from "react"
 import "./mobileGamepad.css"
-export default function MobileGamepad({}){
+export default function MobileGamepad({
+    upArrowPress = () => {},
+    upArrowUp = () => {},
+    upArrowDown = () => {},
+    downArrowPress = () => {},
+    downArrowUp = () => {},
+    downArrowDown = () => {},
+    rightArrowPress = () => {},
+    rightArrowUp = () => {},
+    rightArrowDown = () => {},
+    leftArrowPress = () => {},
+    leftArrowUp = () => {},
+    leftArrowDown = () => {},
+    middlePress = () => {},
+    middleUp = () => {},
+    middleDown = () => {},
+}){
+    const GameArrow = () => {
+        return(
+            <svg className="game-arrow" viewBox="0 -960 960 960" fill="#1f1f1f">
+                <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
+            </svg>
+        )
+    }
 
     return(
-        <div className="column">
-            <div className="gamepad-row">
-                <svg className="game-arrow gamepad-up" viewBox="0 -960 960 960" fill="#1f1f1f">
-                    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
-                </svg>
-            </div>
-            <div className="gamepad-row">
-                <svg className="game-arrow gamepad-left" viewBox="0 -960 960 960" fill="#1f1f1f">
-                    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
-                </svg>
-                <div className="gamepad-mid">
-        
+        <div className="gamepad">
+            <div className="column">
+                <div className="gamepad-row">
+                    <div className="game button gamepad-up">
+                        <GameArrow />
+                    </div>
                 </div>
-                <svg className="game-arrow gamepad-right" viewBox="0 -960 960 960" fill="#1f1f1f">
-                    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
-                </svg>
+                <div className="gamepad-row">
+                    <div className="game button gamepad-left">
+                        <GameArrow />
+                    </div>
+                    <div className="game button gamepad-mid">
+                        <div className="gamepad-middle" />
+                    </div>
+                    <div className="game button gamepad-right">
+                        <GameArrow />
+                    </div>
+                </div>
+                <div className="gamepad-row">
+                    <div className="game button gamepad-down">
+                        <GameArrow />
+                    </div>
+                </div>
             </div>
-            <div className="gamepad-row">
-                <svg className="game-arrow gamepad-down" viewBox="0 -960 960 960" fill="#1f1f1f">
-                    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
-                </svg>   
-            </div>
-            Gamepad works
         </div>
     )
 }
