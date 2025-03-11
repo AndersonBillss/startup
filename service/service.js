d6089a48322e761c2b07477eef39cf8e3dce7f93
AndersonBillss <anderson@thebillsfamily.com>
2025-03-11 17:47:45 -0600

    updated cors policy

--- service/service.js
+++ service/service.js
@@ -7,10 +7,7 @@ const port = 4000
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+app.use(cors({credentials: true}))
 
 app.use('/api', apiRoutes);
 
