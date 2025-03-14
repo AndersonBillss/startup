37232af77d988645cb54d3303a6b9dccef2b33c9
AndersonBillss <anderson@thebillsfamily.com>
2025-03-14 01:09:46 -0600

    seperated database functions into database.js

---
+++ service/database.js
@@ -0,0 +1,149 @@
+const flags = process.argv
+const devEnvironment = flags.indexOf("--configuration=dev") !== -1;
+
+
+
+export const users = [ // Mock data
+    {
+        username: "BenjaminFranklin",
+        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
+        kingdomName: "Green Hill Caliphate",
+        kingdomImg: "https://th.bing.com/th/id/R.ff47da8ee8118d9dc4d394919c9d5fe5?rik=E38d%2bCaqdJ859g&riu=http%3a%2f%2fwww.nationalpedia.com%2fwp-content%2fuploads%2f2017%2f05%2fNational-Flag-of-the-United-States.jpg&ehk=t09O8Cipuyhrsr5CJmbZRUEemqMjuwY%2fVpsZ20N1eGM%3d&risl=&pid=ImgRaw&r=0",
+        unlockedGames: [],
+        soldiers: 999,
+        token: null
+    },
+    {
+        username: "ThomasJefferson",
+        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
+        kingdomName: "Republic of Swift Computers",
+        kingdomImg: "https://fthmb.tqn.com/Q5HpXsZd1Adb9qQ0P0URZbajASA=/3865x2576/filters:fill(auto,1)/us-constitution-175495818-584222b55f9b5851e5878357.jpg",
+        unlockedGames: [],
+        soldiers: 45,
+        token: null
+    },
+    {
+        username: "JuliusCaesar",
+        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
+        kingdomName: "Duchy of Furious Composers",
+        kingdomImg: "https://th.bing.com/th/id/OIP.0FjbbDw0u9lQJVhT8Zkv3gHaEU?rs=1&pid=ImgDetMain",
+        unlockedGames: [],
+        soldiers: 13,
+        token: null
+    },
+    {
+        username: "MikhialGorbachev",
+        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
+        kingdomName: "Humble Sword Dominion",
+        kingdomImg: "https://th.bing.com/th/id/OIP.LKv90-dWJkU7TT-lQDPHDAHaEo?rs=1&pid=ImgDetMain",
+        unlockedGames: [],
+        soldiers: 250,
+        token: null
+    },
+    {
+        username: "GenghisKhan",
+        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
+        kingdomName: "Empire of Good Greatness",
+        kingdomImg: "https://th.bing.com/th/id/R.202bae0e3abc85e993c887332ee7b74f?rik=vTneIZz2RmdjcQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f-_gHEUPycLqk%2fUvn4W0hWR8I%2fAAAAAAAACCI%2fGs8Bb2w1N1A%2fs1600%2fGer%2b-%2bJH%2b(2).jpg&ehk=UTSBdtnCt70mDctsVIFYi99FN9cvjubZJvoDACVi9ck%3d&risl=&pid=ImgRaw&r=0",
+        unlockedGames: [],
+        soldiers: 10000,
+        token: null
+    },
+    {
+        username: "NicholasII",
+        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
+        kingdomName: "Tsardom of Humble Fields",
+        kingdomImg: "https://www.publicdomainpictures.net/pictures/70000/velka/winter-palace-in-st-petersburg.jpg",
+        unlockedGames: [],
+        soldiers: 3,
+        token: null
+    },
+    {
+        username: "TestUser",
+        hashedPassword: "$2b$10$nkNUE4YoR/Lg9F/TIUxCueolFDiMbFdh5EuKhjGArnGOHHaS2KL1m", // Aa!11111
+        kingdomName: "Confederation of Calm Composers",
+        kingdomImg: null,
+        unlockedGames: [],
+        soldiers: 0,
+        token: null
+    },
+]
+
+async function setUserData(username, data){
+    for(const user of users){
+        if(user.username === username){
+            for(const key of Object.keys(data)){
+                user[key] = data[key]
+            }
+            return user
+        }
+    }
+    return false
+}
+
+async function findUser(username){
+    for(const user of users){
+        if(user.username === username){
+            return user
+        }
+    }
+    return false
+}
+async function findAuthorized(token){
+    for(const user of users){
+        if(user.token === token){
+            return user
+        }
+    }
+    return false
+}
+async function getUsers(excludedUsers){
+    function userIncluded(targetUser, excludedUsers){
+        for(const user of excludedUsers){
+            if(user.username === targetUser.username){
+                return true
+            }
+        }
+        return false
+    }
+    const resultUsers = []
+    for(const user of users){
+        if(!userIncluded(user, excludedUsers)){
+            resultUsers.push(limitGetUserData(user))
+        }
+    }
+    return resultUsers
+}
+async function addUser(user){
+    users.push(user)
+}
+
+function limitLoginUserData(user){
+    return{
+        username: user.username,
+        kingdomName: user.kingdomName,
+        kingdomImg: user.kingdomImg,
+        unlockedGames: user.unlockedGames,
+        soldiers: user.soldiers,
+        token: user.token
+    }
+}
+function limitGetUserData(user){
+    return{
+        username: user.username,
+        kingdomName: user.kingdomName,
+        kingdomImg: user.kingdomImg,
+        soldiers: user.soldiers
+    }
+}
+
+const db = {
+    addUser,
+    setUserData,
+    getUsers,
+    findAuthorized,
+    findUser,
+    limitGetUserData,
+    limitLoginUserData
+}
+export default db
--- service/index.js
+++ service/index.js
@@ -4,9 +4,18 @@ import cors from 'cors'
 import cookieParser from "cookie-parser"
 import { v4 as uuid }  from "uuid";
 import bcrypt from 'bcrypt';
+import db from './database.js';
 
+// Default port
+let port = 4000;
+// The service port may be set on the command line
+const flags = process.argv
+flags.forEach(item => {
+  if(item.slice(0,7) === "--port="){
+    port = Number(item.slice(7,item.length))
+  }
+})
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
@@ -21,73 +30,6 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
-
-export const users = [ // Mock data
-    {
-        username: "BenjaminFranklin",
-        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
-        kingdomName: "Green Hill Caliphate",
-        kingdomImg: "https://th.bing.com/th/id/R.ff47da8ee8118d9dc4d394919c9d5fe5?rik=E38d%2bCaqdJ859g&riu=http%3a%2f%2fwww.nationalpedia.com%2fwp-content%2fuploads%2f2017%2f05%2fNational-Flag-of-the-United-States.jpg&ehk=t09O8Cipuyhrsr5CJmbZRUEemqMjuwY%2fVpsZ20N1eGM%3d&risl=&pid=ImgRaw&r=0",
-        unlockedGames: [],
-        soldiers: 999,
-        token: null
-    },
-    {
-        username: "ThomasJefferson",
-        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
-        kingdomName: "Republic of Swift Computers",
-        kingdomImg: "https://fthmb.tqn.com/Q5HpXsZd1Adb9qQ0P0URZbajASA=/3865x2576/filters:fill(auto,1)/us-constitution-175495818-584222b55f9b5851e5878357.jpg",
-        unlockedGames: [],
-        soldiers: 45,
-        token: null
-    },
-    {
-        username: "JuliusCaesar",
-        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
-        kingdomName: "Duchy of Furious Composers",
-        kingdomImg: "https://th.bing.com/th/id/OIP.0FjbbDw0u9lQJVhT8Zkv3gHaEU?rs=1&pid=ImgDetMain",
-        unlockedGames: [],
-        soldiers: 13,
-        token: null
-    },
-    {
-        username: "MikhialGorbachev",
-        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
-        kingdomName: "Humble Sword Dominion",
-        kingdomImg: "https://th.bing.com/th/id/OIP.LKv90-dWJkU7TT-lQDPHDAHaEo?rs=1&pid=ImgDetMain",
-        unlockedGames: [],
-        soldiers: 250,
-        token: null
-    },
-    {
-        username: "GenghisKhan",
-        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
-        kingdomName: "Empire of Good Greatness",
-        kingdomImg: "https://th.bing.com/th/id/R.202bae0e3abc85e993c887332ee7b74f?rik=vTneIZz2RmdjcQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f-_gHEUPycLqk%2fUvn4W0hWR8I%2fAAAAAAAACCI%2fGs8Bb2w1N1A%2fs1600%2fGer%2b-%2bJH%2b(2).jpg&ehk=UTSBdtnCt70mDctsVIFYi99FN9cvjubZJvoDACVi9ck%3d&risl=&pid=ImgRaw&r=0",
-        unlockedGames: [],
-        soldiers: 10000,
-        token: null
-    },
-    {
-        username: "NicholasII",
-        hashedPassword: "$2b$10$ZFgPpm3Pg5NBWrMzJx.M7.ju/whI.0AWlDBOy61ZGuxUt1itJyg9W", // goodPassword1!
-        kingdomName: "Tsardom of Humble Fields",
-        kingdomImg: "https://www.publicdomainpictures.net/pictures/70000/velka/winter-palace-in-st-petersburg.jpg",
-        unlockedGames: [],
-        soldiers: 3,
-        token: null
-    },
-    {
-        username: "TestUser",
-        hashedPassword: "$2b$10$nkNUE4YoR/Lg9F/TIUxCueolFDiMbFdh5EuKhjGArnGOHHaS2KL1m", // Aa!11111
-        kingdomName: "Confederation of Calm Composers",
-        kingdomImg: null,
-        unlockedGames: [],
-        soldiers: 0,
-        token: null
-    },
-]
-
 apiRoutes.get("/ping", (_req, res) => {
     res.send({msg: "Api routes working"})
 })
@@ -98,7 +40,7 @@ apiRoutes.post("/signup", async(req, res) => {
         res.status(400).send({msg: "No username provided"})
         return
     }
-    if(await findUser(username) !== false){
+    if(await db.findUser(username) !== false){
         res.status(400).send({msg: "Username already taken"})
         return
     }
@@ -132,7 +74,7 @@ apiRoutes.post("/signup", async(req, res) => {
         unlockedGames: [],
         soldiers: 0
     }
-    users.push(userSignupObject)
+    db.addUser(userSignupObject)
     res.send({msg: "success"})
 })
 
@@ -147,7 +89,7 @@ apiRoutes.post("/login", async(req, res) => {
         res.status(400).send({msg: "No password sent"})
         return
     }
-    const targetUser = await findUser(username)
+    const targetUser = await db.findUser(username)
     if(targetUser === false){
         res.status(400).send({msg: "Invalid username or password"})
         return
@@ -158,7 +100,7 @@ apiRoutes.post("/login", async(req, res) => {
         return
     }
     setAuthCookie(res, targetUser)
-    res.send({data: limitLoginUserData(targetUser)})
+    res.send({data: db.limitLoginUserData(targetUser)})
 })
 
 apiRoutes.delete("/logout", verifyUser, async(req, res) => {
@@ -171,20 +113,20 @@ apiRoutes.put("/data", verifyUser, async(req, res) => {
         res.status(400).send({msg: "No data provided"})
         return
     }
-    const updated = await setUserData(req.user.username, req.body.data)
+    const updated = await db.setUserData(req.user.username, req.body.data)
     if(updated === false){
         res.status(400).send({msg: "Invalid user id provided"})
         return
     }
-    res.send({data: limitLoginUserData(updated)})
+    res.send({data: db.limitLoginUserData(updated)})
 })
 
 apiRoutes.get("/data", verifyUser, async(req, res) => {
-    res.send({data: limitLoginUserData(req.user)})
+    res.send({data: db.limitLoginUserData(req.user)})
 })
 
 apiRoutes.get("/getUsers", verifyUser, async(req, res) => {
-    const otherUsers = await getUsers([req.user])
+    const otherUsers = await db.getUsers([req.user])
     res.send({data: otherUsers})
 })
 
@@ -199,14 +141,14 @@ apiRoutes.put("/attackUser", verifyUser, async(req, res) => {
         res.status(400).send({msg: "no target id specified"})
         return
     }
-    const targetUser = await findUser(target)
+    const targetUser = await db.findUser(target)
     if(!targetUser){
         res.status(400).send({msg: "Target user doesn't exist"})
         return
     }
     targetUser.soldiers -= soldiers
 
-    const otherUsers = await getUsers([req.user])
+    const otherUsers = await db.getUsers([req.user])
     res.send({data: otherUsers})
 })
 
@@ -214,70 +156,6 @@ app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
 
-async function setUserData(username, data){
-    for(const user of users){
-        if(user.username === username){
-            for(const key of Object.keys(data)){
-                user[key] = data[key]
-            }
-            return user
-        }
-    }
-    return false
-}
-
-async function findUser(username){
-    for(const user of users){
-        if(user.username === username){
-            return user
-        }
-    }
-    return false
-}
-async function findAuthorized(token){
-    for(const user of users){
-        if(user.token === token){
-            return user
-        }
-    }
-    return false
-}
-async function getUsers(excludedUsers){
-    function userIncluded(targetUser, excludedUsers){
-        for(const user of excludedUsers){
-            if(user.username === targetUser.username){
-                return true
-            }
-        }
-        return false
-    }
-    const resultUsers = []
-    for(const user of users){
-        if(!userIncluded(user, excludedUsers)){
-            resultUsers.push(limitGetUserData(user))
-        }
-    }
-    return resultUsers
-}
-
-function limitLoginUserData(user){
-    return{
-        username: user.username,
-        kingdomName: user.kingdomName,
-        kingdomImg: user.kingdomImg,
-        unlockedGames: user.unlockedGames,
-        soldiers: user.soldiers,
-        token: user.token
-    }
-}
-function limitGetUserData(user){
-    return{
-        username: user.username,
-        kingdomName: user.kingdomName,
-        kingdomImg: user.kingdomImg,
-        soldiers: user.soldiers
-    }
-}
 
 function setAuthCookie(res, user) {
     user.token = uuid();
@@ -292,7 +170,7 @@ function setAuthCookie(res, user) {
 // Middleware function
 async function verifyUser(req, res, next){
     const token = req.cookies["token"]
-    const targetUser = await findAuthorized(token)
+    const targetUser = await db.findAuthorized(token)
     if(!targetUser){
         res.status(401).send({ msg: 'Unauthorized' });
         return
